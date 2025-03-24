e11ff63b65052ce163322cd11d67a283bece43e2
Steve Copley <steve.copley@waimea.school.nz>
2025-03-24 13:50:13 +1300

    Update info comment

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,11 @@
  * Kotlin GUI Pop-Up Dialog Demo
  * ===============================================================
  *
+ * This is a demo of how to create and manage a pop-up dialog
+ * window as part of your application.
  *
+ * The pop-up is modal - this means that it retains focus, and the
+ * main window can't be interacted with until the pop-up is closed.
  */
 
 import com.formdev.flatlaf.FlatDarkLaf
